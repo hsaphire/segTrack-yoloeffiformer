d6c30141157686724aa4d8c9dc05d5654e5aca3f
hsaphire <qqaz0952721099@gmail.com>
2023-08-21 15:29:29 +0800

    temp_save3

--- predict.py
+++ predict.py
@@ -1,14 +1,14 @@
-from torch.utils.data import dataset
+import torch.utils.data import dataset
 from tqdm import tqdm
-import network
-import utils
+import DeepLabV3Plus-Pytorch.network
+import DeepLabV3Plus-Pytorch.utils
 import os
 import random
 import argparse
 import numpy as np
 
 from torch.utils import data
-from datasets import VOCSegmentation, Cityscapes, cityscapes
+from DeepLabV3Plus-Pytorch.datasets import data
 from torchvision import transforms as T
 from metrics import StreamSegMetrics
 
@@ -19,17 +19,119 @@ from PIL import Image
 import matplotlib
 import matplotlib.pyplot as plt
 from glob import glob
+######## yolo deeplab cut line ###########
+import time
+from pathlib import Path 
+from yolov7.tracker.byte_tracker import BYTETracker
+import cv2
+import torch.vackends.cudnn as cudnn
+from yolov7.models.experimental import attempt_load
+from yolov7.utils.datasets import LoadStreams, LoadImages
+from yolov7.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from yolov7.utils.plots import plot_one_box
+from yolov7.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from yolov7.utils.visualize import plot_tracking,plot_tracking_route
+from yolov7.utils.timer import Timer
 
-def get_argparser():
+
+class Deeplab():
+    def __init__(pretrained,opt):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.opt = opt
+        self.pretrained = pretrained
+        self.model = network.modeling.__dict__[self.opt.model]
+        if opts.ckpt is not None and os.path.isfile(self.opts.ckpt):
+            self.checkpoint = torch.load(self.opts.ckpt, map_location=torch.device('cpu'))
+            self.model.load_state_dict(checkpoint["model_state"])
+            self.model = nn.DataParallel(model)
+            self.model.to(device)
+            print("Resume model from %s" % self.opts.ckpt)
+            del checkpoint
+        else:
+            print("[!] Retrain")
+            self.model = nn.DataParallel(model)
+            self.model.to(device)
+        with torch.no_grad():
+        model = model.eval()
+        
+    def detect(img_path):
+        if self.opts.crop_val:
+            transform = T.Compose([
+                    T.Resize(opts.crop_size),
+                    T.CenterCrop(opts.crop_size),
+                    T.ToTensor(),
+                    T.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225]),
+                ])
+        else:
+            transform = T.Compose([
+                    T.ToTensor(),
+                    T.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225]),
+                ])
+        
+        ext = os.path.basename(img_path).split('.')[-1]
+            img_name = os.path.basename(img_path)[:-len(ext)-1]
+            img = Image.open(img_path).convert('RGB')
+            img = transform(img).unsqueeze(0) # To tensor of NCHW
+            img = img.to(device)
+            
+            pred = model(img).max(1)[1].cpu().numpy()[0] # HW
+            colorized_preds = decode_fn(pred).astype('uint8')
+            colorized_preds = Image.fromarray(colorized_preds)
+        
+        return colorized_preds
+            
+
+    
+def main(opt):
+    # setup dataloader
+    source,weights,view_img,save_txt,imgsz,trace = opt.source,opt.weights,opt.view_img,opt.save_txt,opt.img_size,not opt.no_trace
+    ave_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    
+    device_yolo = select_device(opt.device)  #device for yolo
+    
+    yolo_model = attempt_load(weights,map_location=device_yolo)
+    stride = int(model.stride.max()) #model stride
+    imgsz = check_img_size(imgsz,s=stride)
+    dataset = LoadImages(source,img_size=imgsz,stride=stride)
+    
+if __name__ == '__main__':
+    
+    # yolo options
     parser = argparse.ArgumentParser()
-
-    # Datset Options
-    parser.add_argument("--input", type=str, required=True,
-                        help="path to a single image or image directory")
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    parser.add_argument("--track_thresh", type=float, default=0.25, help="tracking confidence threshold")
+    parser.add_argument("--track_buffer", type=int, default=120, help="the frames for keep lost tracks")
+    parser.add_argument("--match_thresh", type=float, default=0.95, help="matching threshold for tracking")
+    parser.add_argument("--mot20", dest="mot20", default=False, action="store_true", help="test mot20.")
+    parser.add_argument(
+        "--aspect_ratio_thresh", type=float, default=1.6,
+        help="threshold for filtering out boxes of which aspect ratio are above the given value."
+    )
+    parser.add_argument('--min_box_area', type=float, default=10, help='filter out tiny boxes')
+     # Deeplab Options
     parser.add_argument("--dataset", type=str, default='voc',
                         choices=['voc', 'cityscapes'], help='Name of training set')
-
-    # Deeplab Options
+    
     available_models = sorted(name for name in network.modeling.__dict__ if name.islower() and \
                               not (name.startswith("__") or name.startswith('_')) and callable(
                               network.modeling.__dict__[name])
@@ -54,84 +156,6 @@ def get_argparser():
     
     parser.add_argument("--ckpt", default=None, type=str,
                         help="resume from checkpoint")
-    parser.add_argument("--gpu_id", type=str, default='0',
-                        help="GPU ID")
-    return parser
-
-def main():
-    opts = get_argparser().parse_args()
-    if opts.dataset.lower() == 'voc':
-        opts.num_classes = 21
-        decode_fn = VOCSegmentation.decode_target
-    elif opts.dataset.lower() == 'cityscapes':
-        opts.num_classes = 19
-        decode_fn = Cityscapes.decode_target
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = opts.gpu_id
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print("Device: %s" % device)
-
-    # Setup dataloader
-    image_files = []
-    if os.path.isdir(opts.input):
-        for ext in ['png', 'jpeg', 'jpg', 'JPEG']:
-            files = glob(os.path.join(opts.input, '**/*.%s'%(ext)), recursive=True)
-            if len(files)>0:
-                image_files.extend(files)
-    elif os.path.isfile(opts.input):
-        image_files.append(opts.input)
+    ########### option endline ######################
     
-    # Set up model (all models are 'constructed at network.modeling)
-    model = network.modeling.__dict__[opts.model](num_classes=opts.num_classes, output_stride=opts.output_stride)
-    if opts.separable_conv and 'plus' in opts.model:
-        network.convert_to_separable_conv(model.classifier)
-    utils.set_bn_momentum(model.backbone, momentum=0.01)
-    
-    if opts.ckpt is not None and os.path.isfile(opts.ckpt):
-        # https://github.com/VainF/DeepLabV3Plus-Pytorch/issues/8#issuecomment-605601402, @PytaichukBohdan
-        checkpoint = torch.load(opts.ckpt, map_location=torch.device('cpu'))
-        model.load_state_dict(checkpoint["model_state"])
-        model = nn.DataParallel(model)
-        model.to(device)
-        print("Resume model from %s" % opts.ckpt)
-        del checkpoint
-    else:
-        print("[!] Retrain")
-        model = nn.DataParallel(model)
-        model.to(device)
-
-    #denorm = utils.Denormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # denormalization for ori images
-
-    if opts.crop_val:
-        transform = T.Compose([
-                T.Resize(opts.crop_size),
-                T.CenterCrop(opts.crop_size),
-                T.ToTensor(),
-                T.Normalize(mean=[0.485, 0.456, 0.406],
-                                std=[0.229, 0.224, 0.225]),
-            ])
-    else:
-        transform = T.Compose([
-                T.ToTensor(),
-                T.Normalize(mean=[0.485, 0.456, 0.406],
-                                std=[0.229, 0.224, 0.225]),
-            ])
-    if opts.save_val_results_to is not None:
-        os.makedirs(opts.save_val_results_to, exist_ok=True)
-    with torch.no_grad():
-        model = model.eval()
-        for img_path in tqdm(image_files):
-            ext = os.path.basename(img_path).split('.')[-1]
-            img_name = os.path.basename(img_path)[:-len(ext)-1]
-            img = Image.open(img_path).convert('RGB')
-            img = transform(img).unsqueeze(0) # To tensor of NCHW
-            img = img.to(device)
-            
-            pred = model(img).max(1)[1].cpu().numpy()[0] # HW
-            colorized_preds = decode_fn(pred).astype('uint8')
-            colorized_preds = Image.fromarray(colorized_preds)
-            if opts.save_val_results_to:
-                colorized_preds.save(os.path.join(opts.save_val_results_to, img_name+'.png'))
-
-if __name__ == '__main__':
-    main()
+    opts = opt.parse_args()
