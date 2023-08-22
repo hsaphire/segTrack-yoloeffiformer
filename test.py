a462ac636ce9d0fecf14284f25a4af10f42dc0f9
hsaphire <qqaz0952721099@gmail.com>
2023-08-22 11:58:34 +0800

    test_stage1

---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+class deeplab():
+    def __init__(a):
+        #self.model = torch.load(pretrain)
+        self.foo = a
+    def detect(image):
+        #result = self.model(image)
+        print(self.foo)
+        return 0
+    
+
+objjjj = deeplab(10)
+objjjj.detect()
